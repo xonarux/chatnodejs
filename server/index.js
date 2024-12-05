dcf2e709c046997f0a886c9185f10eaa9f805287
Carlos Herrera <carlosherreram91@gmail.com>
2024-12-05 17:53:34 -0500

    Se agregó el envío de mensajes y la recepción de estos en todos los clientes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,34 @@
 import express from "express";
 import logger from "morgan";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 const port = process.env.PORT ?? 3000;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
+
+io.on("connection", (socket) => {
+  console.log("A user has connected !!");
+
+  socket.on("disconnect", () => {
+    console.log("An user has disconnect");
+  });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
 app.use(logger("dev"));
 
 app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/client/index.html");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
